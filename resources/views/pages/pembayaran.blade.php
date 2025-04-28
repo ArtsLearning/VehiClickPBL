2d0bc196a0655ca7dcc548071a7964a352a6bfbb
MRifqy51 <rifqyhidayat51@gmail.com>
2025-04-28 21:25:48 +0700

    ini versi lebih singkat dari navbarnya

--- resources/views/pages/pembayaran.blade.php
+++ resources/views/pages/pembayaran.blade.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+@extends('layouts.app')
 <body class="h-screen overflow-hidden m-0 p-0">
 
     <div class="relative w-screen h-screen overflow-hidden">
@@ -13,5 +7,4 @@
         @include('components.navbar')
 
     </div>
-    </body>
-</html>
+</body>
