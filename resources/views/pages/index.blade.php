4c9cde2995009d3dea7b1c8837c9ed284d4991d0
ArtsLearning <arthursabs354@gmail.com>
2025-04-29 21:46:45 +0700

    nambahin index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<html lang="id">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -94,5 +93,5 @@
     @include('components.footer')
     </div>
 </body>
-</html>
+
 
