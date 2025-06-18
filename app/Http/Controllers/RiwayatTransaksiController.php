71fa64d77872f1b5073263ae2a7af3a3503a01e9
marhaban akbar <fblaster2000@gmail.com>
2025-06-19 00:14:44 +0700

    sedikit perbaikan di backend riwayat

--- app/Http/Controllers/RiwayatTransaksiController.php
+++ app/Http/Controllers/RiwayatTransaksiController.php
@@ -10,13 +10,17 @@ class RiwayatTransaksiController extends Controller
 {
     public function index()
     {
-        $userId = Auth::id(); // ambil ID user yang sedang login
+        // 1. Ambil ID pengguna yang sedang login
+        $userId = Auth::id();
 
-        $riwayat = Transaksi::with('kendaraan')
-            ->where('user_id', $userId)
-            ->orderBy('created_at', 'desc')
-            ->get();
+        // 2. Ambil data dari database dan simpan ke variabel $riwayat_pengguna
+        $riwayat_pengguna = Transaksi::with('kendaraan')
+                            ->where('user_id', $userId)
+                            ->orderBy('created_at', 'desc')
+                            ->get();
 
-        return view('riwayat.index', compact('riwayat'));
+        // 3. Kirim variabel $riwayat_pengguna ke view dengan nama 'riwayat'
+        //    Pastikan nama di sini ('riwayat') cocok dengan yang dipakai di file Blade (@forelse ($riwayat as ...))
+        return view('pages.riwayat_pemesanan', ['riwayat' => $riwayat_pengguna]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,52 +10,47 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PembayaranController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\ProfilController;
-use App\Http\Controllers\RiwayatController;
 use App\Http\Controllers\TentangController;
 use App\Http\Controllers\Pesan\ContactController;
 use App\Http\Controllers\RiwayatTransaksiController;
 use App\Http\Controllers\PaymentController;
 
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', [IndexController::class,'show']);
-
 Route::get('/change_password', [UbahSandiController::class,'show']);
-
 Route::get('/daftar', [DaftarController::class,'show']);
-
 Route::get('/detail_kendaraan', [DetailKendaraanController::class,'show']);
-
-// Route::get('/login', [LoginController::class,'show']);
-
 Route::get('/pembayaran', [PembayaranController::class,'show']);
-
 Route::get('/produk', [BarangController::class,'showProduct']);
 Route::get('/produk/{id}', [BarangController::class, 'showDetails'])->name('produk.detail');
-
 Route::get('/profil', [ProfilController::class,'show']);
-Route::get('/riwayat', [RiwayatController::class, 'show'])->name('riwayat');
 Route::get('/tentang', [TentangController::class,'show']);
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/pembayaran/proses', [PaymentController::class, 'process'])->name('payment.process');
+
+// Route untuk Riwayat Transaksi yang benar
+Route::get('/riwayat', [RiwayatTransaksiController::class, 'index'])->middleware('auth')->name('riwayat.index');
 
 // Pesan didalam footer
 Route::post('/kontak', [ContactController::class, 'store'])->name('pesan.simpan');
 
 Route::get('/dashboard', [BarangController::class,'showDashboard'])
-->middleware(['auth', 'verified'])->name('dashboard');
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Riwayat Transaksi
-    Route::get('/riwayat-transaksi', [RiwayatTransaksiController::class, 'index'])->name('riwayat.index');
 });
 
 // Tambahkan temporary di routes/web.php untuk debug
